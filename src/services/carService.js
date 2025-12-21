76ae27a29135432c5675e54f78bbd7fd15615200
Huzaifa Afraz <huzaifaafraz90@gmail.com>
2025-12-21 19:32:28 +0500

    add null header in get cars public route

--- src/services/carService.js
+++ src/services/carService.js
@@ -3,7 +3,11 @@ export const carService = {
   createCar: (formData) => apiClient.post('/cars', formData, {
     headers: { 'Content-Type': 'multipart/form-data' }
   }),
-  getCars: (params) => apiClient.get('/cars', { params }),
+  getCars: (params) => apiClient.get('/cars', { params,
+    headers: {
+    Authorization: ''
+  }
+   }),
   getCarDetails: (carId) => apiClient.get(`/cars/${carId}`),
   getMyCars: () => apiClient.get('/cars/me'),
 };
